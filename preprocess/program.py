75908a73b16b1870441642749fa61e5224b36156
Eros <1713464+zeeros@users.noreply.github.com>
2020-06-28 15:41:06 +0200

    Test passing of Labels param

--- preprocess/program.py
+++ preprocess/program.py
@@ -10,7 +10,7 @@ args = parser.parse_args()
 # Creating the directory where the output file will be created (the directory may or may not exist).
 #Path(args.output1_path).parent.mkdir(parents=True, exist_ok=True)
 
-feature_columns, labels, train_dfs = preprocess.load_data(
+feature_columns, labels, train_dfs, test_dfs = preprocess.load_data(
     data_path = "/usr/src/app/data",
     train_csv = ['UDPLag.csv', 'Syn.csv', 'DrDoS_UDP.csv', 'DrDoS_NetBIOS.csv', 'DrDoS_MSSQL.csv', 'DrDoS_LDAP.csv'],
     chunk_size=9**6
