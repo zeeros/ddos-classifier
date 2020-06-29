ada54635466056fcd989a6dd380a3f685315535d
Eros <1713464+zeeros@users.noreply.github.com>
2020-06-29 11:37:45 +0200

    Training component

---
+++ train/preprocess.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import tensorflow as tf
+import zipfile
+import json
+import collections
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def __preprocess_dataframe(df, features, metadata=None):
+    """
+    If metadata is passed, return also the labels
+    """
+    # Trim columns name, replace whitespaces from columns name
+    df = df.rename(columns=lambda x: x.strip().replace(" ", "_"))
+    # Keep only features and label
+    df = df[features + ["Label"]]
+    if metadata is None:
+        # Remove label column from features
+        df.pop('Label')
+    else:
+        # Labels between training and set dataset may be different, map them using metadata
+        labels = df['Label'].unique()
+        for l in labels:
+            if l in metadata['Label']['01-12->03-11']:
+                df['Label'].replace(to_replace=l, value=metadata['Label']['01-12->03-11'][l], inplace=True)
+    return df
+
+
+def __get_features(archive, metadata):
+    """
+    Retrieve the features from the archive
+    """
+    features = []
+    # Get a flat list of all best features
+    for dataset_label in metadata['Label']['Features']:
+        features += metadata['Label']['Features'][dataset_label]
+    features = list(set(features))
+    features = [fc.replace(" ", "_") for fc in features]
+    # Use the first csv file in the archive to extract column features
+    file = next((file for file in archive.namelist() if file.endswith(".csv")), None)
+    df = __preprocess_dataframe(pd.read_csv(archive.open(file), dtype={85: str}), features)
+    feature_columns = []
+    for key in df.keys():
+        feature_columns.append(tf.feature_column.numeric_column(key=key))
+    return feature_columns
+
+
+def load_data(data_path=".", train_csv=None, test_csv=None, chunk_size=10 ** 10):
+    labels = ["BENIGN", "Syn", "UDPLag", "UDP", "LDAP", "MSSQL", "NetBIOS", "WebDDoS"]
+    LoadedData = collections.namedtuple("LoadedData", "feature_columns labels train_df test_df")
+
+    logging.debug('Load metadata')
+    with open(data_path + "/metadata.json") as metadata_file:
+        metadata = json.load(metadata_file)
+
+    train_df = None
+    feature_columns = None
+    if train_csv is not None:
+        logging.debug('Load training dataset...')
+        train_archive = zipfile.ZipFile(data_path + "/CSV-01-12.zip", 'r')
+        # Feature columns describe how to use the input
+        feature_columns = __get_features(train_archive, metadata)
+        train_sets = []
+        for file in train_archive.namelist():
+            if any(file.endswith(t) for t in train_csv):
+                logging.debug('     > Load %s', file)
+                df = __preprocess_dataframe(
+                    df=pd.read_csv(
+                        train_archive.open(file),
+                        dtype={85: str}
+                    ),
+                    features=[fc.key.replace(" ", "_") for fc in feature_columns],
+                    metadata=metadata
+                )
+                # Load csv to dataframe
+                train_sets.append(df)
+        # Merge the dataframes into a single one and shuffle it, random_state assures reproducibility
+        logging.debug('     Merge dataframes and shuffle')
+        train_df = pd.concat(train_sets).sample(frac=1, random_state=1)
+
+    test_dfs = None
+    if test_csv is not None:
+        logging.debug('Load testing dataset...')
+        test_archive = zipfile.ZipFile(data_path + "/CSV-03-11.zip", 'r')
+        # Feature columns describe how to use the input
+        if feature_columns is None:
+            feature_columns = __get_features(test_archive, metadata)
+        test_dfs = []
+        for file in test_archive.namelist():
+            if any(file.endswith(t) for t in test_csv):
+                logging.debug('     > Load %s', file)
+                file_test_dfs = []
+                for chunk in pd.read_csv(test_archive.open(file), dtype={85: str}, chunksize=chunk_size):
+                    df = __preprocess_dataframe(
+                        chunk,
+                        features=[fc.key.replace(" ", "_") for fc in feature_columns],
+                        metadata=metadata
+                    )
+                    file_test_dfs.append({
+                        "labels": df.pop("Label"),
+                        "features": df
+                    })
+                test_dfs.append({
+                    "file": file,
+                    "dataframe": file_test_dfs
+                })
+
+    logging.debug('Loading completed')
+    return LoadedData(feature_columns, labels, train_df, test_dfs)
+
+def load_dataset(csvs, zipfile_path, metadata_path, random_state):
+
+    logging.debug('Loading metadata...')
+    with open(metadata_path) as metadata_file:
+        metadata = json.load(metadata_file)
+
+    logging.debug('Opening archive...')
+    archive = zipfile.ZipFile(zipfile_path, 'r')
+
+    feature_columns = __get_features(archive, metadata)
+    sets = []
+    for file in archive.namelist():
+        if any(file.endswith(t) for t in csvs):
+            logging.debug('     > Load %s', file)
+            df = __preprocess_dataframe(
+                df = pd.read_csv(
+                    archive.open(file),
+                    dtype={85: str}
+                ),
+                features = [fc.key.replace(" ", "_") for fc in feature_columns],
+                metadata = metadata
+            )
+            sets.append(df)
+    # Merge the dataframes into a single one and shuffle it, random_state assures reproducibility
+    logging.debug('Merging and shuffling...')
+    return pd.concat(sets).sample(frac=1, random_state=random_state)
---
+++ train/program.py
@@ -0,0 +1,52 @@
+import preprocess
+import argparse
+from pathlib import Path
+
+# Defining and parsing the command-line arguments
+parser = argparse.ArgumentParser(description='Training component for the DDoS classifier')
+parser.add_argument('--input-dataset-path', type=str, help='Path to the preprocessed dataset')
+parser.add_argument('--output-model-path', type=str, help='Path to the trained model')
+args = parser.parse_args()
+
+# Get dataframe
+df = pd.read_csv(args.input_dataset_path, dtype={85: str}), features
+# Get features
+feature_columns = [tf.feature_column.numeric_column(key=key) for key in df.keys()]
+# Get labels
+labels = ["BENIGN", "Syn", "UDPLag", "UDP", "LDAP", "MSSQL", "NetBIOS", "WebDDoS"]
+# Instantiate the model
+classifier = tf.estimator.DNNClassifier(
+        hidden_units=[60, 30, 20],
+        feature_columns=feature_columns,
+        n_classes=len(labels),
+        label_vocabulary=labels,
+        batch_norm=True,
+        optimizer=lambda: tf.keras.optimizers.Adam(
+            learning_rate=tf.compat.v1.train.exponential_decay(
+                learning_rate=0.1,
+                global_step=tf.compat.v1.train.get_global_step(),
+                decay_steps=10000,
+                decay_rate=0.96)
+        )
+)
+
+def input_fn(df, batch_size=32):
+    '''
+    An input function for training or evaluating
+    '''
+    # Convert the inputs to a Dataset
+    dataset = tf.data.Dataset.from_tensor_slices((dict(df["features"]), df["labels"]))
+    # Shuffle and repeat if you are in training mode
+    dataset = dataset.shuffle(1000).repeat()
+    return dataset.batch(batch_size)
+
+# Train the model
+#for train_df in train_dfs:
+classifier.train(input_fn=lambda: input_fn(df), steps=10**4)
+
+# Creating the directory where the output file will be created (the directory may or may not exist).
+Path(args.output_model_path).parent.mkdir(parents=True, exist_ok=True)
+
+# Save the model
+serving_input_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(tf.feature_column.make_parse_example_spec(feature_columns))
+estimator_path = classifier.export_saved_model(output_model_path, serving_input_fn)
