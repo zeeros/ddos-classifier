c9a29c007e28bcbb3e07f800b415bf127a867293
Eros <1713464+zeeros@users.noreply.github.com>
2020-07-01 08:30:26 +0200

    Refactoring

---
+++ pipeline/compiler.py
@@ -0,0 +1,18 @@
+import os
+import kfp
+from kfp.components import InputPath, InputTextFile, OutputPath, OutputTextFile
+from kfp.components import func_to_container_op
+from typing import NamedTuple
+
+preprocess = kfp.components.load_component_from_file(os.path.join('preprocess_component.yaml'))
+train = kfp.components.load_component_from_file(os.path.join('train_component.yaml'))
+
+@kfp.dsl.pipeline(
+    name='DDoS Attacks Classifier',
+    description='End-to-end training of a traffic classifier'
+)
+def pipeline():
+    preprocess_task = preprocess()
+    train_task = train(preprocess_task.output)
+
+kfp.compiler.Compiler().compile(pipeline, 'pipeline.zip')
