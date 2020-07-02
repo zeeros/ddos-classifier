836f9ee2af69a7b16d7e1c9db367e452eb599806
Eros <1713464+zeeros@users.noreply.github.com>
2020-07-02 10:44:10 +0200

    Split images

--- pipeline/compiler.py
+++ pipeline/compiler.py
@@ -6,6 +6,7 @@ from typing import NamedTuple
 
 preprocess = kfp.components.load_component_from_file(os.path.join('preprocess_component.yaml'))
 train = kfp.components.load_component_from_file(os.path.join('train_component.yaml'))
+test = kfp.components.load_component_from_file(os.path.join('test_component.yaml'))
 
 @kfp.dsl.pipeline(
     name='DDoS Attacks Classifier',
@@ -14,5 +15,6 @@ train = kfp.components.load_component_from_file(os.path.join('train_component.ya
 def pipeline():
     preprocess_task = preprocess()
     train_task = train(preprocess_task.output)
+    test_task = test(train_task.output)
 
 kfp.compiler.Compiler().compile(pipeline, 'pipeline.zip')
