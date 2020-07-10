972df278bf01f81973fb34a4386b91cb7c3b99cb
Eros <1713464+zeeros@users.noreply.github.com>
2020-07-10 08:36:33 +0200

    New version of pipeline

--- env/preprocess-test/preprocess.py
+++ env/preprocess-test/preprocess.py
@@ -131,7 +131,7 @@ def load_dataset(zipfile_path, metadata_path, random_state, csvs=None):
     for file in archive.namelist():
         # Load all the files if csvs parameter is None
         # otherwise check that the file is listed in csvs
-        if (csvs is None) or any(file.endswith(t) for t in csvs):
+        if (csvs is None and file.endswith(".csv")) or any(file.endswith(t) for t in csvs):
             logging.debug('     > Loading %s...', file)
             df = __preprocess_dataframe(
                 df = pd.read_csv(
