e73467075507c1e667c65a1be9df02e89110374c
Eros <1713464+zeeros@users.noreply.github.com>
2020-07-10 21:17:34 +0200

    New version of pipeline

--- env/test/test.py
+++ env/test/test.py
@@ -5,113 +5,34 @@ import pandas as pd
 import tensorflow as tf
 from pathlib import Path
 import logging
-import os
-import json
-import zipfile
 
 logging.basicConfig(level=logging.DEBUG)
 
-def preprocess_dataframe(df, features, metadata=None):
-    """
-    If metadata is passed, return also the labels
-    """
-    # Trim columns name, replace whitespaces from columns name
-    df = df.rename(columns=lambda x: x.strip().replace(" ", "_"))
-    # Keep only features and label
-    df = df[features + ["Label"]]
-    if metadata is None:
-        # Remove label column from features
-        df.pop('Label')
-    else:
-        # Labels between training and set dataset may be different, map them using metadata
-        labels = df['Label'].unique()
-        for l in labels:
-            if l in metadata['Label']['01-12->03-11']:
-                df['Label'].replace(to_replace=l, value=metadata['Label']['01-12->03-11'][l], inplace=True)
-    return df
-
-def get_features(archive, metadata):
-    """
-    Retrieve the features from the archive
-    """
-    features = []
-    # Get a flat list of all best features
-    for dataset_label in metadata['Label']['Features']:
-        features += metadata['Label']['Features'][dataset_label]
-    features = list(set(features))
-    features = [fc.replace(" ", "_") for fc in features]
-    # Use the first csv file in the archive to extract column features
-    file = next((file for file in archive.namelist() if file.endswith(".csv")), None)
-    df = preprocess_dataframe(pd.read_csv(archive.open(file), dtype={85: str}), features)
-    feature_columns = []
-    for key in df.keys():
-        feature_columns.append(tf.feature_column.numeric_column(key=key))
-    return feature_columns
-
-def load_dataset(csvs, zipfile_path, metadata_path, random_state=None):
-
-    logging.debug('Loading metadata...')
-    with open(metadata_path) as metadata_file:
-        metadata = json.load(metadata_file)
-
-    logging.debug('Opening archive...')
-    archive = zipfile.ZipFile(zipfile_path, 'r')
-
-    feature_columns = get_features(archive, metadata)
-
-    dfs = []
-    for file in archive.namelist():
-        if any(file.endswith(t) for t in csvs):
-            logging.debug('     > Loading %s...', file)
-            file_dfs = []
-            for chunk in pd.read_csv(archive.open(file), dtype={85: str}, chunksize=9**6):
-                df = preprocess_dataframe(
-                    chunk,
-                    features=[fc.key.replace(" ", "_") for fc in feature_columns],
-                    metadata=metadata
-                )
-                file_dfs.append({
-                    "labels": df.pop("Label"),
-                    "features": df
-                })
-            dfs.append({
-                "file": file,
-                "dataframe": file_dfs
-            })
-    return dfs
-
 # Defining and parsing the command-line arguments
-parser = argparse.ArgumentParser(description='Training component for the DDoS classifier')
-parser.add_argument('--input-model-path', type=str, help='Path to the trained model')
+parser = argparse.ArgumentParser(description='Testing component for the DDoS classifier')
+parser.add_argument('--input-dataset-path', type=str, help='Path to the preprocessed dataset')
+parser.add_argument('--output-model-path', type=str, help='Path to the trained model')
+parser.add_argument('--output-model-path', type=str, help='Path to the trained model')
 args = parser.parse_args()
 
+# Get dataframe
+df = pd.read_csv(args.input_dataset_path, dtype={85: str})
+
+# Get features
+feature_columns = [tf.feature_column.numeric_column(key=key) for key in df.keys() if key != "Label" ]
+# Get labels
+labels = ["BENIGN", "Syn", "UDPLag", "UDP", "LDAP", "MSSQL", "NetBIOS", "WebDDoS"]
 # Get the only folder inside the path, containing the model
 input_model_path = args.input_model_path + "/" + os.listdir(args.input_model_path)[0]
-
 # Load the DNN models
 model = tf.saved_model.load(input_model_path)
 
-dfs = load_dataset(
-    csvs = ['Syn.csv', 'UDPLag.csv', 'UDP.csv', 'LDAP.csv', 'MSSQL.csv', 'NetBIOS.csv'],
-    zipfile_path = "/usr/src/app/data/CSV-03-11.zip",
-    metadata_path = "/usr/src/app/data/metadata.json"
-)
-
-def predict(x):
-  example = tf.train.Example()
-  example.features.feature["x"].float_list.value.extend([x])
-  return imported.signatures["predict"](
-    examples=tf.constant([example.SerializeToString()]))
-
 # Test the model
-metrics = []
-for file_df in dfs:
-  file_measures = []
-  for df in file_df["dataframe"]:
-    file_measures.append(model.evaluate(input_fn=lambda: input_fn(df, training=False)))
-  metrics.append({
-      "file": file_df["file"],
-      "measures": file_measures
-  })
 
-print(metrics)
+# Creating the directory where the output file will be created (the directory may or may not exist).
+Path(args.output_model_path).parent.mkdir(parents=True, exist_ok=True)
+
+# Save the model
+logging.debug("Saving model...")
+serving_input_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(tf.feature_column.make_parse_example_spec(feature_columns))
+estimator_path = classifier.export_saved_model(export_dir_base=args.output_model_path, serving_input_receiver_fn=serving_input_fn, experimental_mode=ModeKeys.EVAL)
--- pipeline_v2/compiler.py
+++ pipeline_v2/compiler.py
@@ -4,17 +4,19 @@ from kfp.components import InputPath, InputTextFile, OutputPath, OutputTextFile
 from kfp.components import func_to_container_op
 from typing import NamedTuple
 
-preprocess_train_ds = kfp.components.load_component_from_file(os.path.join('preprocess_train_ds_component.yaml'))
-preprocess_test_ds = kfp.components.load_component_from_file(os.path.join('preprocess_test_ds_component.yaml'))
-train_test = kfp.components.load_component_from_file(os.path.join('train_test_component.yaml'))
+preprocess_train = kfp.components.load_component_from_file(os.path.join('preprocess_train_component.yaml'))
+preprocess_test = kfp.components.load_component_from_file(os.path.join('preprocess_test_component.yaml'))
+train = kfp.components.load_component_from_file(os.path.join('train_component.yaml'))
+test = kfp.components.load_component_from_file(os.path.join('test_component.yaml'))
 
 @kfp.dsl.pipeline(
     name='DDoS Attacks Classifier',
     description='End-to-end training of a traffic classifier'
 )
 def pipeline():
-    preproc_train_ds_task = preprocess_train_ds()
-    preproc_test_ds_task = preprocess_test_ds()
-    train_test_task = train_test(preproc_train_ds_task.output, preproc_test_ds_task.output)
+    preproc_train_task = preprocess_train_()
+    preproc_test_task = preprocess_test()
+    train_task = train(preproc_train_task.output)
+    test_task = test(preproc_test_task.output, train_task.output)
 
 kfp.compiler.Compiler().compile(pipeline, 'pipeline.zip')
