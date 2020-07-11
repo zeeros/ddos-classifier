ee0fc6d4bdefbee33f906126e7c146545fdaf295
Eros <1713464+zeeros@users.noreply.github.com>
2020-07-11 16:36:04 +0200

    Images refactoring

--- env/preprocess-train/preprocess_train.py
+++ env/preprocess-train/preprocess_train.py
@@ -20,7 +20,8 @@ df = preprocess.load_dataset(
     zipfile_path = "/usr/src/app/data/CSV-01-12.zip",
     metadata_path = "/usr/src/app/data/metadata.json",
     random_state = 1,
-    csvs = ['UDPLag.csv', 'Syn.csv', 'DrDoS_UDP.csv', 'DrDoS_NetBIOS.csv', 'DrDoS_MSSQL.csv', 'DrDoS_LDAP.csv']
+    csvs = ['UDPLag.csv', 'Syn.csv', 'DrDoS_UDP.csv', 'DrDoS_NetBIOS.csv', 'DrDoS_MSSQL.csv', 'DrDoS_LDAP.csv'],
+    chunksize=8**6
 )
 
 with open(args.output_dataset_path, 'w') as dataset_file:
