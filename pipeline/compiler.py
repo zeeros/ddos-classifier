f55ffb0d6fee3533743846fb111f0cdb2e576057
Eros <1713464+zeeros@users.noreply.github.com>
2020-07-17 14:34:23 +0200

    New pipeline replacing old one

--- pipeline/compiler.py
+++ pipeline/compiler.py
@@ -1,10 +1,14 @@
+# Install Kubeflow Pipelines SDK. Add the --user argument if you get permission errors.
+#!PIP_DISABLE_PIP_VERSION_CHECK=1 python3 -m pip install 'kfp>=0.1.32.2' --quiet --user
+
 import os
 import kfp
 from kfp.components import InputPath, InputTextFile, OutputPath, OutputTextFile
 from kfp.components import func_to_container_op
 from typing import NamedTuple
 
-preprocess = kfp.components.load_component_from_file(os.path.join('preprocess_component.yaml'))
+preprocess_train = kfp.components.load_component_from_file(os.path.join('preprocess_train_component.yaml'))
+preprocess_test = kfp.components.load_component_from_file(os.path.join('preprocess_test_component.yaml'))
 train = kfp.components.load_component_from_file(os.path.join('train_component.yaml'))
 test = kfp.components.load_component_from_file(os.path.join('test_component.yaml'))
 
@@ -13,8 +17,11 @@ test = kfp.components.load_component_from_file(os.path.join('test_component.yaml
     description='End-to-end training of a traffic classifier'
 )
 def pipeline():
-    preprocess_task = preprocess()
-    train_task = train(preprocess_task.output)
-    test_task = test(train_task.output)
+    preproc_train_task = preprocess_train_()
+    preproc_train_task.set_memory_request('10G')
+    preproc_test_task = preprocess_test()
+    preproc_test_task.set_memory_request('10G')
+    train_task = train(preproc_train_task.output)
+    test_task = test(preproc_test_task.output, train_task.output)
 
 kfp.compiler.Compiler().compile(pipeline, 'pipeline.zip')
