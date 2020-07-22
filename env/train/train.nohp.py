6f1318d52a3449c692e9a9f90cb5e39b08909d85
Eros <1713464+zeeros@users.noreply.github.com>
2020-07-22 19:31:11 +0200

    Add basic hyperparam tuning

--- env/train/train.nohp.py
+++ env/train/train.nohp.py
@@ -47,68 +47,16 @@ def input_fn(df, batch_size=32):
     dataset = dataset.shuffle(1000).repeat()
     return dataset.batch(batch_size)
 
-def run_config(hparams):
-    '''
-    Run a specific configuration
-    '''
-    classifier = tf.estimator.DNNClassifier(
-        hidden_units=hparams['HIDDEN_UNITS'],
-        feature_columns=feature_columns,
-        n_classes=len(labels),
-        label_vocabulary=labels,
-        batch_norm=hparams['BATCH_NORM'],
-        optimizer=lambda: tf.keras.optimizers.Adam(
-            learning_rate=tf.compat.v1.train.exponential_decay(
-            learning_rate=0.1,
-            global_step=tf.compat.v1.train.get_global_step(),
-            decay_steps=10000,
-            decay_rate=0.96)
-        ),
-        config=tf.estimator.RunConfig(
-            save_summary_steps=10**3,
-            tf_random_seed=9,
-            log_step_count_steps=10**5
-        )
-    )
-    train_df_count = int(round*(80/100))
-    # Train over #train_df_count dataframes
-    for train_df in train_dfs[:train_df_count]:
-        classifier.train(input_fn=lambda: input_fn(train_df, training=True), steps=10**4)
-    accuracies = []
-    # Evaluate over the remaining dataframes
-    for evaluate_df in train_dfs[train_df_count:]:
-        accuracies.append(classifier.evaluate(input_fn=lambda: input_fn(evaluate_df, training=False))['accuracy'])
-    return {
-        "hparams": hparams,
-        "classifier": classifier,
-        "accuracy": sum(accuracies)/len(accuracies)
-    }
-
 # Train the model
 logging.debug("Training model...")
 chunk_size = 9**6
 train_dfs = np.split(df, range(chunk_size, math.ceil(df.shape[0] / chunk_size) * chunk_size, chunk_size))
 del df
 round = len(train_dfs)
-
-session_num = 0
-session_run = []
-classifier = None
-
-BATCH_NORM = [True, False]
-HIDDEN_UNITS = [[60, 30, 20], [20, 10]]
-for batch_norm in BATCH_NORM:
-  for hidden_units in HIDDEN_UNITS:
-    hparams = {}
-    hparams['BATCH_NORM'] = batch_norm,
-    hparams['HIDDEN_UNITS'] = hidden_units
-    logging.debug("Session #%d" % session_num)
-    logging.debug('hparams: %s', hparams)
-    model = run_config(hparams)
-    if classifier is None or classifier["accuracy"] < model["accuracy"]:
-        # Set current model as the classifier to export
-        classifier = model
-    session_num += 1
+for train_df in train_dfs:
+    logging.debug("     > Rounds to do: %i", round)
+    classifier.train(input_fn=lambda: input_fn(train_df), steps=10**4)
+    round -= 1
 
 # Creating the directory where the output file will be created (the directory may or may not exist).
 Path(args.output_model_path).parent.mkdir(parents=True, exist_ok=True)
